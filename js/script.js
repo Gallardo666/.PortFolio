8adc63bb107b51fec4e2ec379ec9c314dd69677b
Jordi Gallardo <jordigallardo0621@gmail.com>
2024-01-09 19:59:24 +0100

    Añadiendo Electricista a soy:

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin:'right' });
 
 /*======================= typed js =======================*/
 const typed =new Typed ('.multiple-text', {
-    strings: ['Programador','Diseñador Grafico','YouTuber'],
+    strings: ['Programador','Electricista','Diseñador Grafico','YouTuber'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:100,
