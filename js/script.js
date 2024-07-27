06449101a5ec8799eb5d7c656238721147ebe11f
Jordi Gallardo <jordigallardo0621@gmail.com>
2024-07-27 10:43:50 +0200

    Añadiendo imagenes en AutoCad

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin:'right' });
 
 /*======================= typed js =======================*/
 const typed =new Typed ('.multiple-text', {
-    strings: ['Soldador','Programador','Electricista','Diseñador Grafico'],
+    strings: ['Soldador','Programador','Electricista'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:100,
